c62d754e07b274e44f134fb719ba3d45f6ab34b9
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-02-10 14:28:18 +1100

    Create function that creates divs within a grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const grid = document.getElementById("container");
+
+function makeGrid (num) {
+    for (let i = 0; i < num; i++) {
+        // make a row
+        const row = document.createElement("div");
+        row.className = "row";
+        grid.appendChild(row);
+        for (let j = 0; j < num; j++) {
+            // create squares within rows
+            const square = document.createElement("div");
+            square.className = "square";
+            row.appendChild(square);
+        }
+    }
+}
+
+makeGrid(16);
