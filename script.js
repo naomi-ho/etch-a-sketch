e5ad30e59f0eb3ed476763014df88e2eef537db4
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-03-31 14:05:38 +1100

    Add rainbow mode functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const DEFAULT_DIMENSION = 16;
 const grid = document.getElementById("grid");
 const dimensionRange = document.getElementById("dimension-range");
 const dimensionValue = document.getElementById("dimension-value");
+const colorSwitch = document.getElementById("colorSwitch");
 
 function makeGrid (dimension) {
     for (let i = 0; i < dimension; i++) {
@@ -14,11 +15,15 @@ function makeGrid (dimension) {
             // create squares within rows
             const square = document.createElement("div");
             square.className = "square";
-            // changes div's color when hovered over
-            square.onmouseenter = function() {mouseEnter()};
+            square.onmouseenter = function() {mouseEnter(square)};
 
-            function mouseEnter() {
-                square.style.backgroundColor = "white";
+            function mouseEnter(square) {
+                if (colorSwitch.checked) {
+                    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+                    square.style.backgroundColor = randomColor;
+                } else {
+                    square.style.backgroundColor = "white";
+                }
             }
 
             row.appendChild(square);
